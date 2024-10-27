b75830bde3ef8ad2ed870225eaaed04010433641
Roni bhakta <77425964+ronibhakta1@users.noreply.github.com>
2024-10-27 18:53:13 +0530

    db setup

--- Version1.1/src/index.ts
+++ Version1.1/src/index.ts
@@ -2,8 +2,34 @@ import { Hono } from 'hono'
 
 const app = new Hono()
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
+
+app.post('/api/v1/signup', (c) => {
+	return c.text('signup route')
+})
+
+app.post('/api/v1/signin', (c) => {
+	return c.text('signin route')
+})
+
+// app.get('/api/v1/echo/:id', (c) => {
+// 	const id = c.req.param('id')
+// 	console.log(id);
+// 	return c.text('get echo route')
+// })
+
+app.post('/api/v1/home', (c) => {
+
+	return c.text('signin route')
+})
+
+app.put('/api/v1/home/bulkecho', (c) => {
+	return c.text('signin route')
 })
 
 export default app
+//
+
+
+//DIRECT_URL="<YOUR_DATABASE_CONNECTION_STRING>"
+
+
